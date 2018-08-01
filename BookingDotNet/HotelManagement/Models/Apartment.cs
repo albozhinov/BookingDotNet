5a9e9e39fd62d85c4b68db065c40cdcadf1a2587
a.bozhinov90@gmail.com <a.bozhinov90@gmail.com>
2018-08-01 11:41:24 +0300

    Fill apartment class

--- BookingDotNet/HotelManagement/Models/Apartment.cs
+++ BookingDotNet/HotelManagement/Models/Apartment.cs
@@ -8,6 +8,44 @@ namespace HotelManagement.Models
 {
     class Apartment : IApartment
     {
-        
+        // Fields
+        private bool fullyQuipped;
+        private int bedrooms;
+        private int bathrooms;
+        private int onFloor;
+        private int capacity;
+        private int beds;
+        private bool forSmokers;
+        private ViewType view;
+        private List<IExtra> listOfExtras;
+
+        // Constructors
+        public Apartment()
+        {
+
+        }
+
+        public bool FullyQuipped => throw new NotImplementedException();
+
+        public int Bedrooms => throw new NotImplementedException();
+
+        public int Bathrooms => throw new NotImplementedException();
+
+        public int OnFloor => throw new NotImplementedException();
+
+        public int Capacity => throw new NotImplementedException();
+
+        public int Beds => throw new NotImplementedException();
+
+        public bool ForSmokers => throw new NotImplementedException();
+
+        public ViewType View => throw new NotImplementedException();
+
+        public List<IExtra> ListOfExtras => throw new NotImplementedException();
+
+        public decimal CalculatePrice()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
