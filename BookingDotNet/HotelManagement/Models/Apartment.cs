589ea9b843051495c8c7cc2beabdf503a91dbca0
a.bozhinov90@gmail.com <a.bozhinov90@gmail.com>
2018-08-01 14:01:19 +0300

    Modified Apartment class

--- BookingDotNet/HotelManagement/Models/Apartment.cs
+++ BookingDotNet/HotelManagement/Models/Apartment.cs
@@ -3,57 +3,52 @@ using HotelManagement.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Utility;
+
 
 namespace HotelManagement.Models
 {
-    class Apartment : IApartment
+    class Apartment : AccomodationProperty, IApartment
     {
-        // Fields
         private bool fullyQuipped;
         private int bedrooms;
         private int bathrooms;
         private int onFloor;
-        private int capacity;
-        private int beds;
-        private bool forSmokers;
-        private ViewType view;
-        private List<IExtra> listOfExtras;
 
-        // Constructors
+        // Constructor
         public Apartment()
         {
 
         }
 
         // Properties
-        public bool FullyQuipped => throw new NotImplementedException();
-
-        public int Bedrooms => throw new NotImplementedException();
-
-        public int Bathrooms => throw new NotImplementedException();
-
-        public int OnFloor => throw new NotImplementedException();
-
-        public int Capacity => throw new NotImplementedException();
-
-        public int Beds => throw new NotImplementedException();
-
-        public bool ForSmokers => throw new NotImplementedException();
-
-        public ViewType View => throw new NotImplementedException();
-
-        public List<IExtra> ListOfExtras => throw new NotImplementedException();
-
-        public decimal BasePrice => throw new NotImplementedException();
-
-        public void AddExtra(IExtra extra)
+        public bool FullyQuipped
         {
-            throw new NotImplementedException();
+            get => this.fullyQuipped;
+            set => this.fullyQuipped = value;
         }
 
-        public decimal CalculatePrice()
+        public int Bedrooms
         {
-            throw new NotImplementedException();
+            get => this.bedrooms;
+            set
+            {
+                this.bedrooms = value;
+            }
+        }
+
+        public int Bathrooms
+        {
+            get => this.bathrooms;
+            set
+            {
+                this.bathrooms = value;
+            }
+        }
+
+        public int OnFloor
+        {
+            get => this.onFloor;
         }
     }
 }
--- BookingDotNet/UserManagement/Models/NaturalLoyal.cs
+++ BookingDotNet/UserManagement/Models/NaturalLoyal.cs
@@ -11,6 +11,7 @@ namespace UserManagement.Models
         // Fields
         private decimal discount;
 
+        // Constructor
         public NaturalLoyal(string firstName, string lastName, DateTime dateOfBirth, DateTime registeredOn, int numberOfVisits, string telephoneNimber, string email, decimal distcount)
             : base(firstName, lastName, dateOfBirth, registeredOn, numberOfVisits, telephoneNimber, email)
         {
@@ -24,6 +25,7 @@ namespace UserManagement.Models
             set
             {
                 Validation.NumberBorderCheck(0.05M, 0.2M, value, Constants.discount);
+                this.discount = value;
             }
         }
     }
