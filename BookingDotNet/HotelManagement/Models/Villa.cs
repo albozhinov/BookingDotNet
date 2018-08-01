400ffd3631da11517cdd51232f734c17a6079dc3
dragomirm <dragomir.milanov@gmail.com>
2018-08-01 10:27:10 +0300

    Added class Villa.

---
+++ BookingDotNet/HotelManagement/Models/Villa.cs
@@ -0,0 +1,32 @@
+﻿using HotelManagement.Common;
+using HotelManagement.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagement.Models
+{
+    class Villa : IVilla
+    {
+        private int NumberOfFloors;
+
+        private int Bedrooms ;
+
+        private int Bathrooms ;
+
+        private int Capacity ;
+
+        private int Beds ;
+
+        private bool ForSmokers ;
+
+        private ViewType View ;
+
+        private List<RegularExtras> ListOfExtras ;
+
+        public decimal CalculatePrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
