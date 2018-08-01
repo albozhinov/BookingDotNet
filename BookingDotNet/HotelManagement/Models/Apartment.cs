cc377729f587e78a3332eb430392286b35ce1165
a.bozhinov90@gmail.com <a.bozhinov90@gmail.com>
2018-08-01 11:25:53 +0300

    Added class Apartment

---
+++ BookingDotNet/HotelManagement/Models/Apartment.cs
@@ -0,0 +1,13 @@
+﻿using HotelManagement.Common;
+using HotelManagement.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagement.Models
+{
+    class Apartment : IApartment
+    {
+        
+    }
+}
