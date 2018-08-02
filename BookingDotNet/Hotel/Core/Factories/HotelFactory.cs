c715da6f8651b5b7dfcbdfc7ee5233aa454d05cf
a.bozhinov90@gmail.com <a.bozhinov90@gmail.com>
2018-08-02 10:52:56 +0300

    Added Hotel commands assembly

---
+++ BookingDotNet/Hotel/Core/Factories/HotelFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Core.Factories
+{
+    class HotelFactory : IHotelFactory
+    {
+    }
+}
---
+++ BookingDotNet/Hotel/Engine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hotel
+{
+    public class Engine
+    {
+    }
+}
