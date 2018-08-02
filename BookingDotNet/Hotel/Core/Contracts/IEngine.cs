3533d40fe94e28b82a377f530c4d708fc5c80962
dragomirm <dragomir.milanov@gmail.com>
2018-08-02 12:56:50 +0300

    Filled in the IEngine interface.

--- BookingDotNet/Hotel/Core/Contracts/IEngine.cs
+++ BookingDotNet/Hotel/Core/Contracts/IEngine.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using HotelManagement.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UserManagement.Contracts;
 
 namespace Hotel.Core.Contracts
 {
     interface IEngine
     {
+        void Start();
+
+        IReader Reader { get; set; }
+
+        IWriter Writer { get; set; }
+
+        IParser Parser { get; set; }
+
+        IList<IClient> Clients { get; }
+        IList<IHotel> Hotels { get; }
+        IList<IAccomodationProperty> Rooms { get; }
     }
 }
